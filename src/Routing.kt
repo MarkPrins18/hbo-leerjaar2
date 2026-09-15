5b9bd643bcca7d3202d8e60a27b8503184a9c293
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2026-09-15 14:53:40 +0200

    fixed tracking of .IDEA file

--- src/Routing.kt
+++ src/Routing.kt
@@ -3,9 +3,11 @@ package com.example
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.plugins.swagger.*
 
 fun Application.configureRouting() {
     routing {
+        swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
         get("/") {
             call.respondText("Hello, World!")
         }
