f3a924b5274d0849a02f6b7bf1bfe5911122206d
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2026-09-20 15:58:43 +0200

    Let op! Mysql moet ingesteld zijn en de env moet het wachtwoord bevatten.

--- src/Exposed.kt
+++ src/Exposed.kt
@@ -1,58 +1,13 @@
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
-import io.ktor.server.request.receive
-import io.ktor.server.response.respond
-import io.ktor.server.routing.delete
-import io.ktor.server.routing.get
-import io.ktor.server.routing.post
-import io.ktor.server.routing.put
-import io.ktor.server.routing.routing
 import org.jetbrains.exposed.v1.jdbc.Database
 
 fun Application.configureExposed() {
     val config = environment.config
-    val database = Database.connect(
+    Database.connect(
         url = config.property("database.url").getString(),
         driver = "com.mysql.cj.jdbc.Driver",
         user = config.property("database.user").getString(),
         password = config.property("database.password").getString(),
     )
-    val userService = ExposedUserService(database).also {
-        it.createSchema()
-    }
-
-    routing {
-        // Create user
-        post("/users") {
-            val user = call.receive<ExposedUser>()
-            val id = userService.create(user)
-            call.respond(HttpStatusCode.Created, id)
-        }
-
-        // Read user
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toUInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = userService.read(id)
-            if (user != null) {
-                call.respond(HttpStatusCode.OK, user)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        }
-
-        // Update user
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toUInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = call.receive<ExposedUser>()
-            userService.update(id, user)
-            call.respond(HttpStatusCode.NoContent)
-        }
-
-        // Delete user
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toUInt() ?: throw IllegalArgumentException("Invalid ID")
-            userService.delete(id)
-            call.respond(HttpStatusCode.NoContent)
-        }
-    }
 }
