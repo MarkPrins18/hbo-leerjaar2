df13995815c88e4a54ce4df8f935c28622595f97
David Vreugdenhil <d.vreugdenhil@safetyct.com>
2026-09-22 15:37:33 +0200

    Added try catch so the application doesn't crash when DB fails to connect.

--- src/Exposed.kt
+++ src/Exposed.kt
@@ -4,10 +4,13 @@ import org.jetbrains.exposed.v1.jdbc.Database
 
 fun Application.configureExposed() {
     val config = environment.config
-    Database.connect(
-        url = config.property("database.url").getString(),
-        driver = "com.mysql.cj.jdbc.Driver",
-        user = config.property("database.user").getString(),
-        password = config.property("database.password").getString(),
-    )
+    try {
+        Database.connect(
+            url = config.property("database.url").getString(),
+            driver = "com.mysql.cj.jdbc.Driver",
+            user = config.property("database.user").getString(),
+            password = config.property("database.password").getString(),
+        )
+    } catch (_: Exception) {
+    }
 }
